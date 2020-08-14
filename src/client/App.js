1476fbe0aec09490651fe2c33f2acf07dddf353c
Mitchell Schooler <mitchell@schooler.me>
2020-08-14 05:08:00 -0700

    Cleaning up functions

--- src/client/App.js
+++ src/client/App.js
@@ -45,39 +45,34 @@ const App = () => {
 
   addEventListener('keydown', (e) => {
     keysDown[e.keyCode] = true;
-    console.log('key: ', e.keyCode);
+    // console.log('key: ', e.keyCode);
   }, false);
 
   addEventListener('keyup', (e) => {
     delete keysDown[e.keyCode];
   }, false);
 
-  const update = function (modifier) {
+  // Update
+  // -----------------//
+  const update = (map) => {
     if (38 in keysDown || 75 in keysDown) { // Player holding Up
-      hero.y -= hero.speed * modifier;
+      hero.y -= 1;
     }
 
     if (40 in keysDown || 74 in keysDown) { // down
-      hero.y += hero.speed * modifier;
+      hero.y += 1;
     }
     if (37 in keysDown || 72 in keysDown) { // left
-      hero.x -= hero.speed * modifier;
+      hero.x -= 1;
     }
 
     if (39 in keysDown || 76 in keysDown) { // right
-      hero.x += hero.speed * modifier;
+      hero.x += 1;
     }
-
+    // TODO needs boundary checking somewhere here
     Object.keys(keysDown).forEach((key) => {
       delete keysDown[key];
     });
-  };
-  // Draw game
-  // -----------------//
-  const drawGame = () => {
-    if (heroReady) {
-      ctx.drawImage(heroImage, 0, 0, 16, 16, hero.x, hero.y, 16, 16);
-    }
   };
 
   // Tile picker
@@ -85,11 +80,28 @@ const App = () => {
   const tilePicker = (tile, x, y) => {
     // 10x10 grid of sprites
     // bgimage image is 160*160
+
+    // tilepicker needs all tiles
+    // bgimage alone is 100 tiles
+    // most of these can be named? How much automation should this have
+    // I want this function to draw based on name. Maybe have one more thing, object of mappings to tileset
     switch (tile) {
       case 'torch':
         // converts x/y to 16tile
         // this should go somewhere higher up, confuses input
         ctx.drawImage(bgImage, 0, 144, 16, 16, x * 16, y * 16, 16, 16);
+        break;
+      case 'leftWall':
+        ctx.drawImage(bgImage, 0, 0, 16, 16, x * 16, y * 16, 16, 16);
+        break;
+      case 'rightWall':
+        ctx.drawImage(bgImage, 80, 0, 16, 16, x * 16, y * 16, 16, 16);
+        break;
+      case 'topWall':
+        ctx.drawImage(bgImage, 17, 0, 16, 16, x * 16, y * 16, 16, 16);
+        break;
+      case 'bottomWall':
+        ctx.drawImage(bgImage, 17, 80, 16, 16, x * 16, y * 16, 16, 16);
         break;
       default:
         console.log('Tile: ', tile, ', Not found.');
@@ -100,6 +112,29 @@ const App = () => {
   // Tile maps
   // -----------------//
   const dungeonMap = () => {
+    // temporary, ideally we get map from server
+    // define our map
+    const map = {
+      bounds: [],
+    };
+
+    // ugly loops to make walls
+    for (let i = 0; i < 30; i++) {
+      map.bounds.push({ x: 0, y: i, tile: 'leftWall' });
+      map.bounds.push({ x: 29, y: i, tile: 'rightWall' });
+    }
+    for (let i = 0; i < 30; i++) {
+      map.bounds.push({ x: i, y: 0, tile: 'topWall' });
+      map.bounds.push({ x: i, y: 29, tile: 'bottomWall' });
+    }
+
+    return map;
+  };
+
+  // Draw game
+  // -----------------//
+  const drawGame = (map) => {
+    // gen floor
     if (bgReady) {
       for (let x = 0; x < 30; x++) {
         for (let y = 0; y < 30; y++) {
@@ -107,16 +142,21 @@ const App = () => {
         }
       }
 
-      tilePicker('torch', 5, 5);
+      for (let i = 0; i < map.bounds.length; i++) {
+        tilePicker(map.bounds[i].tile, map.bounds[i].x, map.bounds[i].y);
+      }
+    }
+
+    if (heroReady) {
+      ctx.drawImage(heroImage, 0, 0, 16, 16, hero.x * 16, hero.y * 16, 16, 16);
     }
   };
 
   // Reset
   // -----------------//
-  // Sets initial state of objects
   const reset = () => {
-    hero.x = canvas.width / 2;
-    hero.y = canvas.height / 2;
+    hero.x = 10;
+    hero.y = 10;
   };
 
   // Main
@@ -125,13 +165,13 @@ const App = () => {
     const now = Date.now();
     const delta = now - then;
 
+    const map = dungeonMap();
     // updates character movement
-    update(1);
+    update(map);
     // Draws background
     // Runs on every loop
-    dungeonMap();
     // Draws objects to canvas
-    drawGame();
+    drawGame(map);
     then = now;
     // loops on animation frame
     requestAnimationFrame(main);
@@ -142,9 +182,12 @@ const App = () => {
   reset();
 
   main();
+
   return (
     <div>
-      <img src={tiles} />
+      {/* }
+    <img src={tiles} />
+    { */}
     </div>
   );
 };
